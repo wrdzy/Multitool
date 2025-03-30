4c8671f58b9fefe79727053edd3d2ca3bc5945d4
wrdzy <James_NB@outlook.com>
2025-03-30 13:34:09 +0100

    Update Tool.py

--- Tool.py
+++ Tool.py
@@ -47,7 +47,7 @@ def print_header():
     ░          ░░   ░ ░ ░ ░ ▒    ░   ░
     ░ ░        ░░   ░ ░ ░ ░ ▒    ░   ░
     ░ ░
-     version 124244242.5.6
+     version 1.5.6
     """
     terminal_width = shutil.get_terminal_size().columns
     header_lines = header.split("\n")
